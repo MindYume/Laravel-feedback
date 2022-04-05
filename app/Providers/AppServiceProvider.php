fa44ef78f84ee20fc20de2c9dc93d804464a9624
MindYume <youwatchdream@gmail.com>
2022-04-04 22:47:48 -0600

    Registration and verification

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\URL;
+use Carbon\Carbon;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        VerifyEmail::toMailUsing(function ($notifiable) {
+        
+            // Генерация ссылки для подтверждения письма
+            $verifyUrl = URL::temporarySignedRoute(
+                'verification.verify', Carbon::now()->addMinutes(60), [
+                    'id' => $notifiable->getKey(),
+                    'hash' => sha1($notifiable->getEmailForVerification())
+                ]
+            );
+            
+            // Переменные, которые будут доступны в шаблоне письма
+            $vars = [
+                'url' => $verifyUrl
+            ];
+    
+            return (new MailMessage)->subject($verifyUrl);
+        });
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    You are logged in, {{Auth::user()->name}}!
+
+                    @if (Auth::user()->feedbacks)
+                        <ul>
+                            @foreach (Auth::user()->feedbacks->all() as $feedback)
+                                <li>{{ $feedback->msg_name }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    {{-- {{ __('You are logged in!') }} --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
